e27f57d01e277b9cf8b7f63cc9951e28fc30e1ac
emilanovic1 <emilanovic1@etf.unsa.ba>
2018-12-04 16:34:52 +0100

    ucitajgradove

--- src/Tutorijal.java
+++ src/Tutorijal.java
@@ -1,5 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Tutorijal {
     public static void main(String[] args){
 
+    }
+    ArrayList<Grad> ucitajGradove(){
+        Scanner ulaz;
+        try {
+            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+        }
     }
 }

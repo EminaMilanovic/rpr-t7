7ed6f869882f16c78bdd01b3ba71b7986f0624f9
emilanovic1 <emilanovic1@etf.unsa.ba>
2018-12-11 01:49:51 +0100

    ucitajgradove

--- src/Tutorijal.java
+++ src/Tutorijal.java
@@ -5,14 +5,42 @@ import java.util.Scanner;
 
 public class Tutorijal {
     public static void main(String[] args){
-
+        ArrayList<Grad> gradovi=new ArrayList<Grad>();
+        gradovi=ucitajGradove();
     }
-    ArrayList<Grad> ucitajGradove(){
-        Scanner ulaz;
+    private static ArrayList<Grad> ucitajGradove(){
+        ArrayList<Grad> gradovi=new ArrayList<Grad>();
+        int brojac=0;
+        Scanner ulaz = null;
         try {
-            ulaz = new Scanner(new FileReader("mjerenja.txt"));
+            ulaz = new Scanner(new FileReader("mjerenja.txt")).useDelimiter("[\\r\\n,]");
         } catch (FileNotFoundException e) {
-            System.out.println("Datoteka ulaz.txt ne postoji ili se ne može otvoriti");
+            System.out.println("Datoteka mjerenja.txt ne postoji ili se ne može otvoriti");
         }
+        try {
+
+            while(ulaz.hasNext()) {
+                if(brojac==1000) break;
+                brojac++;
+                String s = ulaz.next();
+                System.out.print("ovo je "+s);
+                int i=0;
+                double [] niz=new double[1000];
+                ulaz.next().charAt(0);
+                while(ulaz.hasNextDouble()){
+                    if(i==1000) break;
+                    niz[i]=ulaz.nextDouble();
+                    System.out.print(niz[i]);
+                    i++;
+                }
+
+                gradovi.add(new Grad());
+
+
+            }
+        }catch(Exception e){
+            System.out.print("problem");
+        }
+        return gradovi;
     }
 }
